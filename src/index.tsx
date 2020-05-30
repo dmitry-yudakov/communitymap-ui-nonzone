2a489162075622151be8c4ba632f1d08d001b4f8
rudensergey <rudensergey@gmail.com>
2020-05-31 01:59:12 +0300

    create main screen interface + add component library

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './interface/App.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   // <React.StrictMode>
   <App />,
   // </React.StrictMode>
+
+  // Interface by Sergey
+
   document.getElementById('root')
 );
 
