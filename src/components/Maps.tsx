b4efa2890c4933d43b1dcdd07c2ce08c27b4589e
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-05-31 06:19:56 +0300

    Changed central pin on map

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -7,7 +7,20 @@ import darkStyle from './MapsGoogleDarkStyle.json';
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY || '';
 
 const PinImg = () => (
-  <img style={{ display: 'block' }} alt="here" src="/pin.png" />
+  // <img style={{ display: 'block' }} alt="here" src="/pin.png" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="white"
+    width="45px"
+    height="45px"
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path
+      fill="#79CAB5"
+      d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"
+    />
+  </svg>
 );
 
 export const MapItem: React.FC<{ lat: number; lng: number }> = ({
